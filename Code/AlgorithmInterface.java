29754448015569fca5b66bfea40b646684c98a99
Segade <72915947+Segade@users.noreply.github.com>
2024-07-03 13:37:00 +0200

    Adding code files

---
+++ Code/AlgorithmInterface.java
@@ -0,0 +1,11 @@
+package ai.djl.examples.Ivan;
+
+import java.util.List;
+
+public interface AlgorithmInterface {
+    public static   String calculateDepth(List<ObjectDetected> listOfObjects) {
+
+        System.out.println("Method to be implemented in the algorithm classes");
+        return "";
+    }
+} // end of interface
